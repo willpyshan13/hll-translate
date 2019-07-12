e0b1c6d78602416c6c8ead0501451c098c3fc45d
pengyushan <pengyushan@vv.cn>
2019-07-12 23:41:16 +0800

    1、添加launcher model

---
+++ module_launcher/src/main/java/com/hll/launcher/mvp/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hll.launcher.mvp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hll.launcher.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
