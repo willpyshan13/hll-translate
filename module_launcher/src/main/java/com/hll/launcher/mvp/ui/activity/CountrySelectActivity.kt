4cc94f1e8143b91fc4c2fad807dcf637b7ce20ab
will <545512533@qq.com>
2019-09-24 00:08:37 +0800

    1、地址选择

--- module_launcher/src/main/java/com/hll/launcher/mvp/ui/activity/CountrySelectActivity.kt
+++ module_launcher/src/main/java/com/hll/launcher/mvp/ui/activity/CountrySelectActivity.kt
@@ -2,6 +2,12 @@ package com.hll.launcher.mvp.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.jess.arms.base.BaseActivity
 import com.jess.arms.di.component.AppComponent
@@ -13,6 +19,8 @@ import com.hll.launcher.mvp.contract.CountrySelectContract
 import com.hll.launcher.mvp.presenter.CountrySelectPresenter
 
 import com.hll.launcher.R
+import kotlinx.android.synthetic.main.launcher_country_item.view.*
+import kotlinx.android.synthetic.main.launcher_country_select.*
 
 
 /**
@@ -39,14 +47,33 @@ class CountrySelectActivity : BaseActivity<CountrySelectPresenter>(), CountrySel
 
 
     override fun initView(savedInstanceState: Bundle?): Int {
-        return R.layout.activity_country_select
+        return R.layout.launcher_country_select
     }
 
 
     override fun initData(savedInstanceState: Bundle?) {
+        launcher_country_recycle.layoutManager = LinearLayoutManager(this)
+        launcher_country_recycle.adapter = RecycleAdapter()
 
     }
 
+    class RecycleAdapter :RecyclerView.Adapter<RecycleViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecycleViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.launcher_country_item,null)
+            return RecycleViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return 5
+        }
+
+        override fun onBindViewHolder(holder: RecycleViewHolder, position: Int) {
+            holder.textView.launcher_country_item_tv.text ="111111"
+        }
+
+    }
+
+    class RecycleViewHolder(val textView: View) : RecyclerView.ViewHolder(textView)
 
     override fun showLoading() {
 
